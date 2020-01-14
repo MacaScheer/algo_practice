6ea01ad1905b73869bfe9656665a7e2f9289b91c
Michael_Scheer <macas.scheer@gmail.com>
2020-01-13 22:13:52 -0800

    sorted array to BST

---
+++ convertSortedArrtoBST.js
@@ -0,0 +1,73 @@
+ function TreeNode(val) {
+     this.val = val;
+     this.left = this.right = null;
+ }
+ 
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function(nums, root = new TreeNode()) {
+    if (!nums.length) return null;
+    let midIdx = Math.floor(nums.length / 2)
+    let secHalf = nums.slice(midIdx + 1)
+    let firstHalf = nums.slice(0, midIdx)
+    tree.root = nums[midIdx]
+    tree.root.left = new TreeNode()
+    
+};
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    insert(val, root = this.root) {
+        if (!this.root) {
+            this.root = new TreeNode(val)
+            return
+        }
+        if (val < root.val) {
+            if (!root.left) {
+                root.left = new TreeNode(val)
+            } else {
+                this.insert(val, root.left)
+            }
+        }
+        else {
+            if (!root.right) {
+                root.right = new TreeNode(val)
+            } else {
+                this.insert(val, root.right)
+            }
+            
+        }
+
+    }
+    bstArr() {
+        const arr = [];
+        const queue = [this.root]
+        while (queue.length) {
+            let node = queue.shift()
+            arr.push(node.val)
+            if (node.left) queue.push(node.left)
+            if(node.right) queue.push(node.right)
+        }
+        return arr
+    }
+}
+
+console.log(sortedArrayToBST([-10,-3,0,5,9]))
+
+// var insertBST = function()
+
+// root of tree should be middle of array
+
+// Given the sorted array: [-10,-3,0,5,9],
+
+// One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+//       0
+//      / \
+//    -3   9
+//    /   /
+//  -10  5
