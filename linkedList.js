128614e6c1efb206804b0fedf78f5bc1bee84af8
Michael_Scheer <macas.scheer@gmail.com>
2020-02-03 21:51:03 -0800

    doubly linked list algos

--- linkedList.js
+++ linkedList.js
@@ -25,12 +25,47 @@ class DoublyLinkedList {
         prevTail.next = this.tail
     }
 
-    insertBefore(node, nodeToInsert) {
+    insertBefore(node, nodeToInsert) { // o--O-  -o- -O--
         // Write your code here.
+        let prevPrev = node.prev;
+        prevPrev.next = nodeToInsert;
+        nodeToInsert.next = node;
+        nodeToInsert.prev = prevPrev;
+        node.prev = nodeToInsert;
     }
 
     insertAfter(node, nodeToInsert) {
         // Write your code here.
+        let prevNext = node.next;
+        node.next = nodeToInsert;
+        nodeToInsert.next = prevNext;
+        prevNext.prev = nodeToInsert;
+    }
+
+    insertBefore(node, nodeToInsert) {
+        if (nodeToInsert === this.head && nodeToInsert === this.tail) return;
+        this.remove(nodeToInsert);
+        nodeToInsert.prev = node.prev;
+        nodeToInsert.next = node;
+        if (node.prev === null) {
+            this.head = nodeToInsert
+        } else {
+            node.prev.next = nodeToInsert
+        }
+        node.prev = nodeToInsert
+    }
+
+    insertAfter(node, nodeToInsert) {
+        if (nodeToInsert === this.head && nodeToInsert === this.tail) return;
+        this.remove(nodeToInsert);
+        nodeToInsert.prev = node
+        nodeToInsert.next = node.next;
+        if (node.next === null) {
+            this.tail = nodeToInsert
+        } else {
+            node.next.prev = nodeToInsert
+        }
+        node.next = nodeToInsert
     }
 
     insertAtPosition(position, nodeToInsert) {
