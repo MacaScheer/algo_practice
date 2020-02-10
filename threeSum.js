e110e246f4a6c36bc41d7ef7508fd436583059fa
Michael_Scheer <macas.scheer@gmail.com>
2020-02-10 13:41:08 -0800

    fixed param values: realized that the sorting algorithm changes in the input array length

--- threeSum.js
+++ threeSum.js
@@ -13,45 +13,71 @@ function mergeSort(arr) {
 }
 
 
-function threeNumberSum(array, targetSum) {
-    let sortedArr = mergeSort(array)
-    console.log("sortedArr: ", sortedArr)
-    let sumsArr = [];
-    let cur = 0;
-    let i = cur + 1;
-    let j = array.length - 1;
-    while (cur < array.length) {
-        let l = sortedArr[i]
-        let r = sortedArr[j]
-        let currNum = sortedArr[cur] 
-        let currSum = currNum + l + r;
-        console.log("i: ", i, "  j: ", j)
-        console.log("currNum: ", currNum, "  l:",l, "   r:", r, "  currSum: ", currSum,)
-        if (i > j) {
-            cur += 1;
-            i = cur + 1;
-            j = sortedArr.length - 1
+// function threeNumberSum(array, targetSum) {
+//     let sortedArr = mergeSort(array)
+//     console.log("sortedArr: ", sortedArr)
+//     let sumsArr = [];
+//     let cur = 0;
+//     let i = cur + 1;
+//     let j = array.length - 1;
+//     while (cur < array.length) {
+//         let l = sortedArr[i]
+//         let r = sortedArr[j]
+//         let currNum = sortedArr[cur] 
+//         let currSum = currNum + l + r;
+//         console.log("i: ", i, "  j: ", j)
+//         console.log("currNum: ", currNum, "  l:",l, "   r:", r, "  currSum: ", currSum,)
+//         if (i > j) {
+//             cur += 1;
+//             i = cur + 1;
+//             j = sortedArr.length - 1
 
+//         }
+//         if (currSum < targetSum) {
+//             i++
+//             continue;
+//         }
+//         if (currSum > targetSum) {
+//             j--
+//             continue;
+//         }
+//         if (currSum === targetSum) {
+//             sumsArr.push([sortedArr[cur], l, r])
+//             j--;
+//             i++
+//         }
+//     }
+// return sumsArr
+// }
+
+function threeNumberSum(array, targetSum) {
+    let arr = mergeSort(array);
+    let triplets = [];
+    for (let i = 0; i < arr.length; i++) {
+        let left = i + 1;
+        let right = arr.length - 1
+        let cur = arr[i]
+        console.log("arr: ", arr, "cur: ",cur,"  left: ", left, "  right: ", right)
+        while (left < right) {
+            let currSum = cur + arr[left] + arr[right]
+            if (currSum === targetSum) {
+                triplets.push([arr[i], arr[left], arr[right]]);
+                left++
+                right--
+            } else if (currSum < targetSum) {
+                left ++
+            } else if (currSum > targetSum) {
+                right --
+            }
         }
-        if (currSum < targetSum) {
-            i++
-            continue;
-        }
-        if (currSum > targetSum) {
-            j--
-            continue;
-        }
-        if (currSum === targetSum) {
-            sumsArr.push([sortedArr[cur], l, r])
-            j--;
-            i++
-        }
+        return triplets
     }
-return sumsArr
+
 }
 
-console.log(threeNumberSum([12,3,1,2,-6,5,-8,6], 0))
-// console.log(mergeSort([12,3,1,2,-6,5,-8,6]))
+
+// console.log(threeNumberSum([12,3,1,2,-6,5,-8,6], 0))
+console.log(threeNumberSum([1, 2, 3], 6))
 
 
 // console.log(threeNumberSum([1,2,3], 6), "should equal: [[1,2,3]]")
