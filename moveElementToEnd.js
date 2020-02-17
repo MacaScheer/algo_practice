f47753d3bf1985f191f307cb73867c8c2525a143
Michael_Scheer <macas.scheer@gmail.com>
2020-02-17 13:58:47 -0800

    moveElementToEnd algorithm

---
+++ moveElementToEnd.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+'use strict';
+console.log("HERE")
+
+function moveElementToEnd(array, toMove) {
+    let i = 0; let j = array.length - 1;
+    while (i !== j) {
+        let iEl = array[i]
+        let jEl = array[j]
+        if (iEl === toMove && jEl !== toMove) {
+            [array[i], array[j]] = [array[j], array[i]];
+            i++;
+            j--;
+        } else if (iEl === toMove && jEl === toMove) {
+            j--
+        } else if (iEl !== toMove) {
+            i++
+        }
+    }
+    return array
+}
+
+console.log(moveElementToEnd([2,1,2,2,2,3,4,2], 2))
