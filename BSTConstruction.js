00c2d13eb3812797b7f20c64f7c788d57c57633a
Michael_Scheer <macas.scheer@gmail.com>
2020-02-18 11:48:06 -0800

    fixed reassignment errors

--- BSTConstruction.js
+++ BSTConstruction.js
@@ -52,12 +52,7 @@ class BST {
         return node.value
     }
 
-
     remove(value, parent = null) {
-    //first search for the value
-    //if it is a leaf node, make its parent's left/right relation = null
-    //if it has left or right child nodes, find the left most child from the right subtree
-        
         if (value < this.value) {  //traverse the tree until you reach the right place, recursively
             if (this.left !== null) {
                 this.left.remove(value, this)
@@ -74,12 +69,12 @@ class BST {
             } else if (parent === null) {          //if this is the root node and only has a left or a right
                 if (this.left !== null) {          //if it's only child node is the left, 
                     this.value = this.left.value;  //replace all connections and values with left
+                    this.right = this.left.right;  //set right before setting left because right depends on original left
                     this.left = this.left.left;
-                    this.right = this.left.right;
                 } else if (this.right !== null) {  //if it's only child is the right,
                     this.value = this.right.value; //replace all connections and values with right
+                    this.left = this.right.left    //set left before setting right because left depends on original right
                     this.right = this.right.right;
-                    this.left = this.right.left
                 } else {                            //if its a single node tree, set value to null
                     this.value = null;
                 }
