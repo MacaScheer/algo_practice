d2e85f1b4a3d5f812b4b2a2fb380a912f9b057d3
Michael_Scheer <macas.scheer@gmail.com>
2020-02-18 11:40:19 -0800

    add edge case comments

--- BSTConstruction.js
+++ BSTConstruction.js
@@ -13,8 +13,8 @@ class BST {
       if (value < this.value) {
           if (this.left === null) {
               this.left = new BST(value);
-        } else {
-            this.left.insert(value)
+            } else {
+                this.left.insert(value)
             }
         } else {
             if (this.right === null) {
@@ -57,7 +57,8 @@ class BST {
     //first search for the value
     //if it is a leaf node, make its parent's left/right relation = null
     //if it has left or right child nodes, find the left most child from the right subtree
-        if (value < this.value) {
+        
+        if (value < this.value) {  //traverse the tree until you reach the right place, recursively
             if (this.left !== null) {
                 this.left.remove(value, this)
             }
@@ -66,26 +67,27 @@ class BST {
                 this.right.remove(value, this)
             }
         } else {
-            if (this.left !== null && this.right !== null) {
-                this.value = this.right.getMinValue();
-                this.right.remove(this.value, this);
-            } else if (parent === null) {
-                if (this.left !== null) {
-                    this.value = this.left.value;
+            if (this.left !== null && this.right !== null) { //if there are both left and right nodes
+                this.value = this.right.getMinValue();       //replace with smallest value on right subtree
+                this.right.remove(this.value, this);         //remove the smallest
+            
+            } else if (parent === null) {          //if this is the root node and only has a left or a right
+                if (this.left !== null) {          //if it's only child node is the left, 
+                    this.value = this.left.value;  //replace all connections and values with left
                     this.left = this.left.left;
                     this.right = this.left.right;
-                } else if (this.right !== null) {
-                    this.value = this.right.value;
+                } else if (this.right !== null) {  //if it's only child is the right,
+                    this.value = this.right.value; //replace all connections and values with right
                     this.right = this.right.right;
                     this.left = this.right.left
-                } else {
+                } else {                            //if its a single node tree, set value to null
                     this.value = null;
                 }
             }
-            else if (parent.left === this) {
-                parent.left = this.left !== null ? this.left : this.right
+            else if (parent.left === this) {        //if is an only child on the left, with only a left or right child
+                parent.left = this.left !== null ? this.left : this.right  //if it has a left, replace with left
             } else if (parent.right === this) {
-                parent.right = this.left !== null ? this.left : this.right
+                parent.right = this.left !== null ? this.left : this.right //if it has a right, replace with right
             
             }
         }
