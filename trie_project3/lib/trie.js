2616457cecfa12a7e066bd1f2004d51b78d96f73
Michael_Scheer <macas.scheer@gmail.com>
2020-02-25 12:49:18 -0800

    solve wordsWithPrefix algorithm

--- trie_project3/lib/trie.js
+++ trie_project3/lib/trie.js
@@ -45,10 +45,57 @@ class Trie {
         node.isTerminal = true
     }
     searchRecur(word, node = this.root) {
-        if (word.length === 0 && node.isTerminal) {
-           
-       }
-   }
+        if (word.length === 0) {
+            if (node.isTerminal) return true
+            return false
+        }
+        let letter = word[0]
+
+        if (letter in node.children) {
+           return this.searchRecur(word.slice(1), node.children[letter])
+        } else {
+            return false
+        }
+    }
+    
+    searchIter(word) {
+        let wordArr = word.split("")
+        let letter;
+        let node = this.root;
+        while (wordArr.length > 0) {
+            letter  = wordArr.shift()
+            if (node.children[letter]) {
+                node = node.children[letter]
+            } else {
+                return false
+            }
+        }
+        if (node.isTerminal) return true
+        return false
+    }
+
+    wordsWithPrefix(prefix, node = this.root) {
+        if (prefix.length === 0) {
+            let allWords = []
+            if (node.isTerminal) allWords.push('')
+            for (let letter in node.children) {
+                let child = node.children[letter]
+                let suffs = this.wordsWithPrefix('', child)
+                let words = suffs.map(suf => letter + suf)
+                allWords.push(...words)
+            }
+            return allWords
+
+        } else {
+            let letter = prefix[0];
+            if (node.children[letter] !== undefined) {
+                let suffixes = this.wordsWithPrefix(prefix.slice(1), node.children[letter])
+                return suffixes.map(suf => letter + suf);
+            } else {
+                return []
+            }
+        }
+    }
 }
 
 module.exports = {
