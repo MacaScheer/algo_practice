78c78a03c4764c0e4d46970fa394e3f8bd8002bc
Michael_Scheer <macas.scheer@gmail.com>
2020-02-25 16:53:30 -0800

    selection sort

---
+++ practiceAlgos.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+'use strict';
+
+
+// Step 1 − Set MIN to location 0
+// Step 2 − Search the minimum element in the list
+// Step 3 − Swap with value at location MIN
+// Step 4 − Increment MIN to point to next element
+// Step 5 − Repeat until list is sorted
+
+
+function selectionSort(array) {
+    let minIdx = 0;
+    let newMinIdx;
+    while (minIdx < array.length) {
+        newMinIdx = findMin(array, minIdx + 1)
+        if (array[newMinIdx] < array[minIdx]) {
+            [array[minIdx], array[newMinIdx]] = [array[newMinIdx], array[minIdx]];
+        }
+        minIdx++;
+    }
+    return array
+}
+    
+function findMin(array, i) {
+    let min = Infinity;
+    let minIDX;
+    console.log(array)
+    while(i < array.length) {
+        if (array[i] < min) {
+            min = array[i]
+            minIDX = i
+        }
+        i++
+    }
+    return minIDX
+}
+    
+
+
+
+
+console.log(selectionSort([23,7,44,9,8,7,6,5]))
