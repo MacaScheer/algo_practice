bcb4469320f31b14026704e5f0800278d30335d4
Michael_Scheer <macas.scheer@gmail.com>
2020-03-05 15:19:02 -0800

    experiment with nested loops to create all possible combinations of nonadjacent elements

--- maxSubSumNoAdjacents.js
+++ maxSubSumNoAdjacents.js
@@ -7,13 +7,16 @@ const maxSubsetSumNoAdjacent = function(array){
     let startIdx = 0;
     let nonAdjEls = [];
     while (startIdx < array.length) {
-        
-        nonAdjEls.push(...helper(array, startIdx))
-        nonAdjEls.push(...helper2(array, startIdx))
-        // nonAdjElementSums.push(helper(array, startIdx).reduce(sum))
+        let els1 = helper(array, startIdx)
+        let els2 = helper2(array, startIdx)
+        console.log(els1)
+        console.log(els2)
+        // nonAdjEls.push(...els1)
+        // nonAdjEls.push(...els2)
+        nonAdjElementSums.push(sumMer(els1), sumMer(els2))
         startIdx++
     }
-    console.log(nonAdjEls)
+    // console.log(nonAdjEls)
     // console.log("SUMS: ", nonAdjElementSums)
 
 }
@@ -22,7 +25,7 @@ const maxSubsetSumNoAdjacent = function(array){
 const helper = function (array, startIdx = 0) { //[0,1,2,3]
     let outerArr = []
     let increment = 2
-    while (increment < array.length){
+    while (increment < array.length && startIdx + increment < array.length){
         let idx2 = startIdx + increment;
         let innerArr = [array[startIdx]]
 
@@ -36,16 +39,25 @@ const helper = function (array, startIdx = 0) { //[0,1,2,3]
     }
     return outerArr
 }
+
+const sumMer = function (arr) {
+    if (arr.length === 0) return 0;
+    if (arr.length === 1) return arr[0];
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    return sum
+}
 //increment is incremented during inner loop so idxs could be [0,2,5] etc.
 const helper2 = function (array, startIdx = 0) {
     let outerArr = [];
     let increment = 2;
-    while (increment < array.length) {
+    while (increment < array.length && startIdx + increment < array.length) {
         let idx2 = startIdx + increment;
         let innerArr = [array[startIdx]];
 
         while (idx2 < array.length) {
-            console.log("INCREMENT: ", increment, "  idx2: ", idx2, "  innerArr: ", innerArr)
             innerArr.push(array[idx2]);
             increment++
             idx2 += increment;
@@ -55,8 +67,31 @@ const helper2 = function (array, startIdx = 0) {
     return outerArr
 }
 
+const helper3 = function (array) {
+    let retArr = [];
+    let idx1 = 0;
+    while (idx1 < array.length) {
+        //change starting idx
+        let innerArr = [];
+        let inc = 2;
+        while (inc < array.length) {
+            //change increment
+            let idx2 = idx1 + inc;
+            innerArr = [array[idx1]]
+            while (idx2 < array.length) {
+                //increment until end of array
+                innerArr.push(array[idx2])
+                idx2+=inc
+            }
+            inc++
+        }
+        idx1++
+    }
+}
 
 //should not concatenate strings, but sum numbers
 const sum = (a,b) => a += b
 console.log(helper2([75, 105, 120, 75, 90, 135]))
+console.log(helper([75, 105, 120, 75, 90, 135]))
 // console.log(maxSubsetSumNoAdjacent([75,105,120,75,90,135])) //should be 330 [75, 120,135]
+
