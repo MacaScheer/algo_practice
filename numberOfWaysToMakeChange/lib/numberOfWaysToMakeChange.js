7bb9e9f9457f63624da55583dbbe8b5cd3d0e7eb
Michael_Scheer <macas.scheer@gmail.com>
2020-03-06 17:26:57 -0800

    number of ways to make change, algo, given an amount and an array of denominations. Dynamic approach

--- numberOfWaysToMakeChange/lib/numberOfWaysToMakeChange.js
+++ numberOfWaysToMakeChange/lib/numberOfWaysToMakeChange.js
@@ -2,11 +2,41 @@
 'use strict';
 
 
+//create a ways array that is of length n - 1
+//iterate through the denoms:
+   //iterate through the ways array
+        //if denom <= n, then update ways[n] += ways[n - denom]
+
 
 function numberOfWaysToMakeChange(n, denoms) {
 
+
+    let ways = [1];
+    for (let i = 1; i <= n; i++){
+        ways[i] = 0;
+    }
+    console.log("ways: ", ways)
+        for (let i = 0; i < denoms.length; i++) {
+            let denom = denoms[i];
+            console.log("denom: ", denom)
+            for (let amt = 1; amt <= ways.length; amt++){
+                if (denom <= amt) {
+                    ways[amt] += ways[amt - denom]
+                }
+            }
+        }
+        return ways[n]
 }
 
-// console.log(numberOfWaysToMakeChange(25, [1,5,10,25]))  //should equal 13
+
+
+
+
+
+console.log(numberOfWaysToMakeChange(25, [1,5,10,25]))  //should equal 13
+//1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1
+//1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+5
+//1+1+1+1+1+1+1+1+1+1+1+1+1+1+1+5+5
+
 
 module.exports = {numberOfWaysToMakeChange}
