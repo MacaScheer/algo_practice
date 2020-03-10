0254f40308ca941fee9c5ade62cab2dbf089a73c
Michael_Scheer <macas.scheer@gmail.com>
2020-03-10 15:23:32 -0700

    make 2D array to store chars at indices, comparing two strings

--- levenshteinDistance.js
+++ levenshteinDistance.js
@@ -5,9 +5,40 @@ function levenshteinDistance(str1, str2) {
     
 }
 
-console.log(levenshteinDistance('', ''), "should be 0") //should be 0
-console.log(levenshteinDistance('abc', 'abc'), "should be 0") //should be 0
-console.log(levenshteinDistance('', 'abc'), "should be 3") //should be 3
-console.log(levenshteinDistance('abc', 'yabcx'), "should be 2") //should be 2
-console.log(levenshteinDistance('biting', 'mitten'), "should be 4") //should be 0
-console.log(levenshteinDistance('cereal', 'saturday'), "should be 6") //should be 0
+function twoArrMaker(str1, str2) {
+    str2 = ' ' + str2
+    str1 = ' ' + str1
+    
+    let rowArr = str2.split("");
+    let colArr = str1.split("");
+    let outterArr = [];
+    for (let i = 0; i <= rowArr.length; i++){
+        let row = [];
+        for (let j = 0; j <= colArr.length; j++){
+            row.push([])
+        }
+        outterArr.push(row)
+    }
+    console.log(outterArr)
+
+    for (let i = 0; i < outterArr.length; i++){
+        let row = outterArr[i];
+        for (let j = 0; j < row.length; j++){
+            if (str2[i] === str1[j]) {
+                outterArr[i][j] = 1;
+            } else {
+                outterArr[i][j] = 0;
+            }
+        }
+    }
+    console.log(outterArr)
+}
+
+// console.log(levenshteinDistance('', ''), "should be 0") //should be 0
+// console.log(levenshteinDistance('abc', 'abc'), "should be 0") //should be 0
+// console.log(levenshteinDistance('', 'abc'), "should be 3") //should be 3
+// console.log(levenshteinDistance('abc', 'yabcx'), "should be 2") //should be 2
+// console.log(levenshteinDistance('biting', 'mitten'), "should be 4") //should be 0
+// console.log(levenshteinDistance('cereal', 'saturday'), "should be 6") //should be 0
+
+console.log(twoArrMaker('abc','yabcx'))
