fdeb743758ff51cd48974a3282ed3291c8263cab
Michael_Scheer <macas.scheer@gmail.com>
2020-03-09 18:02:43 -0700

    readjusting combinations array

--- minNumCoinsChange.js
+++ minNumCoinsChange.js
@@ -6,17 +6,37 @@ function minNumberOfCoinsForChange(n, denoms) {
     for (let i = 0; i <= n; i++) {
         ways[i] = 0;
     }
-    for (let i = denoms.length - 1; i > 0; i--){
+    denoms = qSort(denoms)
+    console.log(denoms)
+    for (let i = 0; i < denoms.length; i++){
         let denom = denoms[i];
-        for (let j = denom; j < ways.length; j++){
-            if (ways[j] <= denom) {
-                ways[j] += ways[j - denom]
-            }
+        for (let amt = denom; amt <= n; amt++){
+            // if (denom <= amt) {
+            //     ways[amt] += ways[amt - denom]
+            // }
+            let quot = Math.floor(amt / denom);
+            let rem = amt % denom;
+            ways[amt] = quot + ways[rem]
+
+            // console.log("amt: ", amt, " denom: ", denom, "  quot: ", quot, "  rem: ", rem, "  ways[amt]: ", ways[amt])
         }
     }
-    console.log(ways)
+    // console.log(ways)
+    return ways[n]
 }
-console.log(minNumberOfCoinsForChange(7, [1, 5, 10]))
-console.log(minNumberOfCoinsForChange(3, [2,1]))
-console.log(minNumberOfCoinsForChange(4, [1,5,10]))
+
+function qSort(array) {
+    if (array.length <= 1) return array;
+    let piv = array.shift();
+    let left = array.filter(el => el <= piv);
+    let right = array.filter(el => el > piv);
+    let sortedLeft = qSort(left);
+    let sortedRight = qSort(right);
+    return sortedLeft.concat([piv]).concat(sortedRight)
+}
+
+// console.log(qSort([45,7,2,49,3,2,1,77]))
+// console.log(minNumberOfCoinsForChange(11, [1,5,10]))
+// console.log(minNumberOfCoinsForChange(3, [2,1]))
+// console.log(minNumberOfCoinsForChange(4, [1,5,10]))
 console.log(minNumberOfCoinsForChange(135, [39, 45, 130, 40, 4, 1, 60, 75]))
