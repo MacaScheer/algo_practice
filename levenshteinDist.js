ab9ac6473a41a04a560680f4d95eb955dcde25d5
Michael_Scheer <macas.scheer@gmail.com>
2020-04-29 12:23:02 -0700

    another test looking at matrix approach

--- levenshteinDist.js
+++ levenshteinDist.js
@@ -19,3 +19,14 @@ console.log(levenshteinDist("abc", "yabd"), "should be 2")
 //a| |1|0|0|
 //b| |0|1|0|
 //d| |0|0|0|
+
+console.log(levenshteinDist("biting", "mitten"), "should be 3?")
+
+//  _ b i t i n g
+// | | | | | | | |
+//m| |0|0|0|0|0|0|
+//i| |0|1|0|1|0|0|
+//t| |0|0|1|0|0|0|
+//t| |0|0|1|0|0|0|
+//e| |0|0|0|0|0|0|
+//n| |0|0|0|0|1|0|
