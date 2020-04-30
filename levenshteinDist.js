223e3911eeab07a9aac514562b689a4e6076fef6
Michael_Scheer <macas.scheer@gmail.com>
2020-04-30 12:26:07 -0700

    levenshteinDist building out comparison array logic

--- levenshteinDist.js
+++ levenshteinDist.js
@@ -8,21 +8,38 @@ console.log("Levenshtein Distance Algo")
 // as it iterates, building out the substrings, that number can just be added to the previous
 function levenshteinDist(str1, str2) {
     
-    let arr1 = str1.split("");
-    let arr2 = str2.split("");
-    let compArr1 = new Array(arr1.length + 1);
-    let compArr2 = new Array(arr2.length + 1)
+    let arr1 = " ".concat(str1).split("");
+    let arr2 = " ".concat(str2).split("");
     let compareArr = [];
-    for (let i = 0; i <= arr1.length; i++){
-        for (let j = 0; j <= arr2.length; j++){
-            let char1 = arr1[i];
-            let char2 = arr2[j];
 
-            compareArr[i][j]
+    for (let i = 0; i < arr1.length; i++){
+        compareArr.push([])
+        let char1 = arr1[i];
+        for (let j = 0; j < arr2.length; j++){
+            let char2 = arr2[j];
+            if (i > 0 && j > 0) {
+                if (char1 !== char2) {
+                    compareArr[i][j] = Math.min(compareArr[i][j - 1], compareArr[i - 1][j]) + 1
+                } else {
+                    compareArr[i][j] = compareArr[i - 1][j - 1]
+                }
+            } else {
+                if (char1 === char2) {
+                    compareArr[i][j] = 0
+                } else {
+                    compareArr[i][j] = 1
+                }
+            }
+            
         }
     }
-
+    console.log(compareArr)
+    return compareArr[compareArr.length - 1][compareArr[0].length - 1]
 }
+
+// function numDiffs(str1, str2) {
+    
+// }
 
 console.log(levenshteinDist("abc", "yabd"), "should be 2")
 
@@ -33,6 +50,6 @@ console.log(levenshteinDist("abc", "yabd"), "should be 2")
 //   yab| |2|1 |1  |
 //  yabd| |3|2 |2  |
 
-console.log(levenshteinDist("biting", "mitten"), "should be 3?")
+// console.log(levenshteinDist("biting", "mitten"), "should be 4?")
 
 
