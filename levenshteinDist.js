4ba1332294f845cc41cb4b2ebd73833b64297f5f
Michael_Scheer <macas.scheer@gmail.com>
2020-04-30 17:29:14 -0700

    optimized version O(min(M, N))

--- levenshteinDist.js
+++ levenshteinDist.js
@@ -43,7 +43,7 @@ function levenshteinDist(str1, str2) {
             
         }
     }
-    console.log(compareArr)
+    // console.log(compareArr)
     return compareArr[compareArr.length - 1][compareArr[0].length - 1]
 }
 
@@ -51,7 +51,33 @@ function levenshteinDist(str1, str2) {
     
 // }
 
-console.log(levenshteinDist("abc", "yabd"), "should be 2")
+function levDist(str1, str2) {
+    const edits = [];
+    for (let i = 0; i < str2.length + 1; i++){
+        const row = [];
+        for (let j = 0; j < str1.length + 1; j++){
+            row.push(j)
+        }
+        row[0] = i;
+        edits.push(row)
+    }
+
+
+    for (let i = 1; i < str2.length + 1; i++){
+        for (let j = 1; j < str1.length + 1; j++){
+            if (str2[i - 1] === str1[j - 1]) {
+                edits[i][j] = edits[i - 1][j - 1]
+            } else {
+                edits[i][j] = 1 + Math.min(edits[i - 1][j],edits[i][j-1], edits[i - 1][j - 1])
+            }
+        }
+    }
+    return edits[str2.length][str1.length]
+}
+
+
+// console.log(levDist('abc', 'yabd'))
+// console.log(levenshteinDist("abc", "yabd"), "should be 2")
 
 //       _|a|ab|abc
 //      | | |  |   |
@@ -63,3 +89,13 @@ console.log(levenshteinDist("abc", "yabd"), "should be 2")
 // console.log(levenshteinDist("biting", "mitten"), "should be 4?")
 
 
+function levDistOptimized(str1, str2) {
+    const small = str1.length > str2.length ? str2 : str1;
+    const big = str1.length > str2.length ? str1 : str2;
+
+    const evenEdits = [];
+    const oddEdits = new Array(small.length + 1);
+    
+
+}
+console.log(levDistOptimized('biting', 'mitten'), "should be 4")
