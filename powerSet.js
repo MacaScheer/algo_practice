b262a3c4ca3670e607d3cf053631e80bde451089
Michael_Scheer <macas.scheer@gmail.com>
2020-05-05 16:16:58 -0700

    trouble with pointers/refs in manipulating array then iterating said array

--- powerSet.js
+++ powerSet.js
@@ -14,44 +14,91 @@
 // }
 
 // console.log(powerSet([1,2,3]))
-
-function powerSet(array) {
+// THE SET OF ALL SUBSETS OF ANOTHER SET
+// function powerSet(array) {
 	// if (array.length === 0) return [[]];
 	// if (array.length === 1) return [[], array];
-	let outer = [[]]; let i = 0;
-	for (i; i < array.length; i++){
-		let el = [array[i]]
-		// let inner = outer;
-		// console.log("inner: ", inner)
-		// let idxToPush = 
+	// let outer = [[]]; let i = 0;
+	// for (i; i < array.length; i++){
+	// 	let el = [array[i]]
+	// 	// let inner = outer;
+	// 	// console.log("inner: ", inner)
+	// 	// let idxToPush = 
 		
-		// for (let j = 0; j < outer.length; j++){
-		// 	inner[j].push(el)
-		// }
-		outer.push(el)
-	}
-	console.log("outer: ",outer, "  i:",i)
-	let startIdx = 2;
-	for (let j = 0; j < array.length; j++){
-		// let nextOuter = outer  //[[],[1],[2],[3]]
-		let el2 = array[j];
-		// let countUp = startIdx; //2
-		// console.log("nextOuter: ",nextOuter)
-		(function(n, c, o) {
-			while (c < o) {
-				let newSub = n[c]
-				console.log("outer: ", outer)
-				newSub.push(el2)
-				n.push(newSub)
-				c++
-			}
+	// 	// for (let j = 0; j < outer.length; j++){
+	// 	// 	inner[j].push(el)
+	// 	// }
+	// 	outer.push(el)
+	// }
+	// console.log("outer: ",outer, "  i:",i)
+	// let startIdx = 2;
+	// for (let j = 0; j < array.length; j++){
+	// 	// let nextOuter = outer  //[[],[1],[2],[3]]
+	// 	let el2 = array[j];
+	// 	// let countUp = startIdx; //2
+	// 	// console.log("nextOuter: ",nextOuter)
+	// 	(function(n, c, o) {
+	// 		while (c < o) {
+	// 			let newSub = n[c]
+	// 			console.log("outer: ", outer)
+	// 			newSub.push(el2)
+	// 			n.push(newSub)
+	// 			c++
+	// 		}
 			
-		})(outer, startIdx, outer.length)
-		startIdx++
-	}
-	console.log(outer)
+	// 	})(outer, startIdx, outer.length)
+	// 	startIdx++
+// 	}
+// 	console.log(outer)
 
+// }
+
+// function powerSet(array) {
+// 	let base = [[]];
+// 	for (let i = 0; i < array.length; i++){
+// 		let el = array[i];
+// 		let newArr = [...base]  //[[]]
+// 		for (let j = 0; j < newArr.length; j++){
+// 			console.log("newArr: ", newArr)
+// 			console.log("base: ", base)
+// 			let sub = newArr[j]; // []
+// 			sub.push(el)
+// 		}
+// 		base.push(newArr)
+// 	}
+// console.log(base)
+// }
+
+// let arr1 = [[], [], [1]]
+// arr1.map(sub => {
+// 	sub.push(1)
+// 	console.log(sub)
+// })
+// console.log(arr1)
+
+
+function powerSet(array) {
+	let outer = [[]];
+	for (let i = 0; i < array.length; i++){
+		let el = array[i];  //1     //2            //3
+		// console.log("i: ",i)
+		let newSub = []
+		console.log("outer: ", outer)
+		let newOuter = [...outer]
+		newOuter.map(sub => {  //[]    //[], [1]      //[],[1],[2],[1,2]
+			sub.push(el)    //[1]   
+			newSub.push(sub)//[[1]] //[[2], [1,2]] //[[3],[1,3],[2,3],[1,2,3]]
+			console.log("sub: ",sub)
+			console.log("newSub: ", newSub)
+		})
+
+
+
+		outer.push(...newSub) //[[],[1]]  //[[],[1],[2],[1,2]]  // [[],[1],[2],[1,2][3],[1,3],[2,3],[1,2,3]]
+	}
+	return outer
 }
+
 
 console.log(powerSet([1, 2, 3]))
 // [[],[1],[2],[3]...[1,2],[1,3],[2,3],[1,2,3]]
