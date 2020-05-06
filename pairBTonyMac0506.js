cc60d8b55cde84aede8c418b903dbf299ef69522
Michael_Scheer <macas.scheer@gmail.com>
2020-05-06 16:18:44 -0700

    class functions for tree node

--- pairBTonyMac0506.js
+++ pairBTonyMac0506.js
@@ -2,3 +2,24 @@
 'use strict';
 
 console.log("pair boarding session 05/06/2020")
+console.log("Tony's Code: ")
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+ 
+}
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {TreeNode}
+ */
+var trimBST = function(root, L, R) {
+    
+};
+
+
+let A = new TreeNode()
+console.log(trimBST(root1, A, D))
