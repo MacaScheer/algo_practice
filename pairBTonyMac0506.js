224f48b9d3be2317ce7b6837ed75279553414f82
Michael_Scheer <macas.scheer@gmail.com>
2020-05-06 18:06:44 -0700

    find odd element in array of sorted pairs in O(logN) time

--- pairBTonyMac0506.js
+++ pairBTonyMac0506.js
@@ -17,9 +17,58 @@ function TreeNode(val, left, right) {
  * @return {TreeNode}
  */
 var trimBST = function(root, L, R) {
-    
+    if (root === null) return null;
+    if (root.val < L) {
+        
+    }
+    if (root.val > R) {
+        
+    } else {
+
+        return root
+    }
 };
 
 
 let A = new TreeNode()
-console.log(trimBST(root1, A, D))
+// console.log(trimBST(root1, A, D))
+
+// Example 1:
+// Input: [1,1,2,3,3,4,4,8,8]
+// Example 2:
+// Input: [3,3,7,7,10,11,11]
+// Output: 10
+// [1,1,2,2,4,5,5]
+// [1,1,2,3,3,4,4]
+
+
+function oddOneOut(array) {
+    if (array.length === 1) return array[0];
+    let midIdx = Math.floor(array / 2);
+    let midEle = array[midIdx];
+    // same to the right
+    if (midEle === array[midIdx + 1]) {
+        let length = array.length - midIdx;
+        if (length % 2 === 0) {          //if side is even length, search left side
+            oddOneOut(array.slice(0,midIdx))
+        } else {                        //if side if odd length, search right side
+            oddOneOut(array.slice(midIdx))
+        }
+    }
+    //same to the left, i.e.  [1,1,2,2,4,5,5]
+    else if (midEle === array[midIdx - 1]) {
+        let length = midIdx
+        if (length % 2 === 0) {  //search right
+            oddOneOut(array.slice(midIdx))
+        } else {
+            oddOneOut(array.slice(0,midIdx))
+        }
+    } else {
+        return midEle
+    }
+
+}
+let arr1 = [1, 1, 2, 3, 3, 4, 4, 8, 8];
+console.log(oddOneOut(arr1));
+let arr2 = [3, 3, 7, 7, 10, 11, 11];
+console.log(oddOneOut(arr2));
