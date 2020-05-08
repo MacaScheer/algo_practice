bfcd6b63c0c3b55f35a89eb60880462f8a3331bc
Michael_Scheer <macas.scheer@gmail.com>
2020-05-08 13:13:21 -0700

    river sizes functions, nested loop, visited object, and dfs function

--- riverSizes.js
+++ riverSizes.js
@@ -3,6 +3,40 @@
 
 console.log("RIVER SIZES");
 
-function riverSizes() {
-    
+function riverSizes(matrix) {
+    let sizes = [];
+    let visited = {};
+    for (let i = 0; i < matrix.length; i++){
+        for (let j = 0; j < matrix[i].length; j++){
+            let dig = matrix[i][j];
+
+            if (!visited[[i, j]]) {
+
+                if (dig === 1) {
+                    let size = dfs(i, j, 1)
+                    sizes.push(size)
+                }
+
+                visited[[i,j]] = true
+            }
+        }
+        return sizes
+    }
+
+    function dfs(i,j, count) {
+        
+    }
 }
+
+
+function dfs(idx, matrix) { }
+
+
+const m1 = [
+    [1, 0, 0, 1, 0],
+    [1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 0]
+];
+console.log(riverSizes(m1), "should be [1,2,2,2,5]");
