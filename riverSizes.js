f6e4d8a06d91dac61851876c3910c55f0ab8d887
Michael_Scheer <macas.scheer@gmail.com>
2020-05-08 14:16:03 -0700

    change read/write to visited into helper function

--- riverSizes.js
+++ riverSizes.js
@@ -10,21 +10,25 @@ function riverSizes(matrix) {
         for (let j = 0; j < matrix[i].length; j++){
             let dig = matrix[i][j];
 
-            if (!visited[[i, j]]) {
+            
 
                 if (dig === 1) {
                     let size = dfs(i, j, 1)
                     sizes.push(size)
                 }
 
-                visited[[i,j]] = true
-            }
+             
         }
         return sizes
     }
 
     function dfs(i,j, count) {
-        
+        if (!visited[[i, j]]) {
+            visited[[i, j]] = true
+            
+            
+            }
+                // should we just check/write visited in helper function?
     }
 }
 
@@ -39,7 +43,7 @@ const m1 = [
     [1, 0, 1, 0, 1],
     [1, 0, 1, 1, 0]
 ];
-// console.log(riverSizes(m1), "should be [1,2,2,2,5]");
+console.log(riverSizes(m1), "should be [1,2,2,2,5]");
 
 
 function testscope(array1, arr2) {
@@ -56,4 +60,4 @@ function testscope(array1, arr2) {
 const a1 = [1, 2, 3, 4, 5, 6];
 const a2 =
     ["a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"];
-console.log(testscope(a1,a2))
+// console.log(testscope(a1,a2))
