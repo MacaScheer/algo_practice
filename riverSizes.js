c9c16caa9649ee55d1161faaac00081560382f14
Michael_Scheer <macas.scheer@gmail.com>
2020-05-08 16:34:17 -0700

    count not being accumulated through recursive call returns

--- riverSizes.js
+++ riverSizes.js
@@ -27,35 +27,34 @@ function riverSizes(matrix) {
     let visited = {};
     for (let i = 0; i < matrix.length; i++){
         for (let j = 0; j < matrix[i].length; j++){
-            let dig = matrix[i][j];
-
-            
             if (!visited[[i, j]]) {
                 visited[[i, j]] = true
-                let size = dfs(i, j, visited)
-                sizes.push(size)
+                if (matrix[i][j] === 1) {
+                    let size = dfs(i, j, visited, matrix)
+                    sizes.push(size) 
+                }
             }
-                
-                
-
-             
         }
-        return sizes
     }
 
-    function dfs(i,j, count = 0, visited) {
+    function dfs(i,j, visited, matrix, count = 0,) {
+        if (i < 0 || j < 0 || i > matrix.length - 1 || j > matrix[0].length - 1) return count;
+        if (visited[[i, j]] === true) return count;
         visited[[i, j]] = true
         if (matrix[i][j] === 1) {
-            dfs(i - 1, j, count++);
-            dfs(i + 1, j, count++)
-            dfs(i, j - 1, count++);
-            dfs(i, j + 1, count++);
+            // count++
+            
+            dfs(i - 1, j, visited, matrix, count++);
+            dfs(i, j - 1, visited, matrix, count++);
+            dfs(i, j + 1, visited, matrix, count++);
+            dfs(i + 1, j, visited, matrix, count++);
                 
         } else {
             return count;
         }
                 // should we just check/write visited in helper function?
     }
+        return sizes
 }
 
 const m1 = [
