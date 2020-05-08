6fbe07c9175e2e346614f1cf40b2c67f73b7ee58
Michael_Scheer <macas.scheer@gmail.com>
2020-05-08 13:27:22 -0700

    function scope test function, seeing how to break out helper functions and pass variables efficiently

--- riverSizes.js
+++ riverSizes.js
@@ -39,4 +39,21 @@ const m1 = [
     [1, 0, 1, 0, 1],
     [1, 0, 1, 1, 0]
 ];
-console.log(riverSizes(m1), "should be [1,2,2,2,5]");
+// console.log(riverSizes(m1), "should be [1,2,2,2,5]");
+
+
+function testscope(array1, arr2) {
+    
+    for (let i = 0; i < array1.length; i++){
+       cprint(array1[i])
+    }
+
+    function cprint(i) {
+        console.log("i: ",i,)
+        console.log(arr2[i])
+    }
+}
+const a1 = [1, 2, 3, 4, 5, 6];
+const a2 =
+    ["a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"];
+console.log(testscope(a1,a2))
