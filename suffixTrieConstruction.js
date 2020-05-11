37492e824450e81c3a809347a1e8a3ff51d2a6fc
Michael_Scheer <macas.scheer@gmail.com>
2020-05-11 15:52:00 -0700

    wordsWithPrefixes practice

--- suffixTrieConstruction.js
+++ suffixTrieConstruction.js
@@ -32,3 +32,42 @@ test.contains("st");
 test.contains("est");
 test.contains("test");
 test.contains("tes");
+
+class TrieNode{
+    constructor() {
+        this.children = {};
+        this.isTerminal = false;
+    }
+}
+class Trie{
+    constructor(root) {
+        this.root = root;
+    }
+    addWord(string, node = this.root) {
+        if (string.length === 0) {
+            node.isTerminal = true;
+            return;
+        }
+        let chars = string.split("");
+        let firstChar = chars.shift();
+        let nextNode;
+        if (node.children[firstChar]) {
+            nextNode = node.children[firstChar]
+            nextNode.addWord(chars.join(""))
+        } else {
+            node.children[firstChar] = new TrieNode();
+            nextNode = node.children[firstChar]
+            nextNode.addWord(chars.join(""))
+        }
+    }
+    wordsWithPrefix(string) {
+        let allWords = [];
+        if (string.length === 0) {
+            let nodes = this.root.children;
+            for (let node of nodes) {
+                
+                if(node.children)
+            }
+        }
+    }
+}
