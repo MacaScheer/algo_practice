8c499cef7b08ca04fcf5ca6a0a828a614c5f7c7b
Michael_Scheer <macas.scheer@gmail.com>
2020-05-11 15:09:18 -0700

    reconciling suffixTrie with Tries construction, not complete

--- suffixTrieConstruction.js
+++ suffixTrieConstruction.js
@@ -3,3 +3,32 @@
 
 console.log("Suffix Trie Construction");
 
+class SuffixTrie{
+    constructor(string) {
+        this.root = {};
+        this.endSymbol = '*';
+        this.populateSuffixTrieFrom(string);
+    }
+
+    populateSuffixTrieFrom(string) {
+        if (string.length === 1) this.root[this.endSymbol] = new SuffixTrie
+        let chars = string.split("");
+        let firstChar = chars.shift();
+        if (this.root[firstChar]) {
+            this.root[firstChar].populateSuffixTrieFrom(chars)
+        } else {
+
+        }
+    }
+
+    contains(string) {
+        
+    }
+}
+
+let test = new SuffixTrie("test");
+test.contains("t");
+test.contains("st");
+test.contains("est");
+test.contains("test");
+test.contains("tes");
