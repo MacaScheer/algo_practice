2a3735b309cb77242a8efa20a763996c984c7006
Michael_Scheer <macas.scheer@gmail.com>
2020-05-13 12:15:59 -0700

    FEM trees and graphs lecture

--- singleCycleCheck.js
+++ singleCycleCheck.js
@@ -1,39 +1,51 @@
 #!/usr/bin/env node
 'use strict';
 
-const singleCycleCheck = function (array) {
-    let visited = traverse(0, array);
-    for (let key in visited) {
-        if (!visited[key]) return false
-    }
-    return true;
-}
+// const singleCycleCheck = function (array) {
+//     let visited = traverse(0, array);
+//     for (let key in visited) {
+//         if (!visited[key]) return false
+//     }
+//     return true;
+// }
 
-const traverse = function (idx, array, visited = {}) {
+// const traverse = function (idx, array, visited = {}) {
     
-    // FIRST JUMP
+//     // FIRST JUMP
     
-    let val = array[idx];
-    if (visited[idx]) {
-        return visited
-    } else {
-        visited[idx] = true
-    }
-    let newIdx;
-    if (idx + val > array.length - 1) {
-        newIdx = (idx + val) % array.length
-    } else if (idx + val < 0) {
-        newIdx = array.length - ((array.length - (idx + val)) % array.length);
-    } else {
-        newIdx = idx + val
-    }
-    // let nextVal = array[newIdx]
+//     let val = array[idx];
+//     if (visited[idx]) {
+//         return visited
+//     } else {
+//         visited[idx] = true
+//     }
+//     let newIdx;
+//     if (idx + val > array.length - 1) {
+//         newIdx = (idx + val) % array.length
+//     } else if (idx + val < 0) {
+//         newIdx = array.length - ((array.length - (idx + val)) % array.length);
+//     } else {
+//         newIdx = idx + val
+//     }
+//     // let nextVal = array[newIdx]
 
-    // SECOND JUMP
-    traverse(newIdx, array, visited);
+//     // SECOND JUMP
+//     traverse(newIdx, array, visited);
 
+// }
+
+
+const singleCycleCheck = function(array){
+    let counter = 0;
+    const firstEl = array[0];
+    let el = firstEl;
+    while (counter < array.length) {
+        el = array[el]
+
+        counter++
+    }
+    return el === firstEl
 }
-
 
 
 console.log(singleCycleCheck([2,3,1,-4,-4,2]), "should be TRUE")
---
+++ treesGraphsFEMLecture.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+'use strict';
+
+console.log("FEM Graphs and Trees Lecture")
