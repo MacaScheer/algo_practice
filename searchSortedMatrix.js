189cad167f5fac61a0b2215209424f674cb37607
Michael_Scheer <macas.scheer@gmail.com>
2020-05-18 14:01:30 -0700

    hi-level logic pseudocode in print statement

--- searchSortedMatrix.js
+++ searchSortedMatrix.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log(`search sorted matrix: given a two-dimensional array of distinct integers and a target integer, ${"\n"} write a function that returns the indices or row/column of the target number.${"\n"} Each row in the matrix is sorted and each column in the matrix is sorted. ${"\n"} The matrix does not necessarily have equal height and width.`);
-
+console.log(`search sorted matrix: given a two-dimensional array of distinct integers and a target integer, ${"\n"}Write a function that returns the indices or row/column of the target number.${"\n"}Each row in the matrix is sorted and each column in the matrix is sorted. ${"\n"}The matrix does not necessarily have equal height and width.`);
+console.log(`PSEUDOCODE: ${"\n"}I plan on using a sort of 2D binary search method on the matrix so that the search time will be logarithmic`)
 function searchInSortedMatrix(matrix) {
     
 
