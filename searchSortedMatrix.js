a20a779873852cb65283a86f9455c17099637f49
Michael_Scheer <macas.scheer@gmail.com>
2020-05-18 14:43:48 -0700

    find fault in logic, eliminating portions of the array without knowing that they can't contain the target, i.e. a number in a given column can't tell you what other numbers on that same row

--- searchSortedMatrix.js
+++ searchSortedMatrix.js
@@ -3,6 +3,7 @@
 
 console.log(`search sorted matrix: given a two-dimensional array of distinct integers and a target integer, ${"\n"}Write a function that returns the indices or row/column of the target number.${"\n"}Each row in the matrix is sorted and each column in the matrix is sorted. ${"\n"}The matrix does not necessarily have equal height and width.`);
 console.log(`PSEUDOCODE: ${"\n"}I plan on using a sort of 2D binary search method on the matrix so that the search time will be logarithmic`)
+console.log(`CANNOT eliminate a quarter of the matrix on each call, because there isn't a way to tell (if the target is greater/less than the number in the row/col ${"\n"}being looked at, that the number may still be in the same row and not on the next column`)
 function searchInSortedMatrix(matrix, target) {
     let midColIdx = Math.floor(matrix[0].length / 2);
     let midRowIdx = Math.floor(matrix.length / 2);
@@ -13,7 +14,7 @@ function searchInSortedMatrix(matrix, target) {
         let returnVal = searchInSortedMatrix(smallerMatrix, target);
         return returnVal !== [-1,-1] ? [returnVal[0] + midRowIdx + 1, returnVal[1] + midColIdx + 1] : [-1,-1]
     } else if (midEle < target) {
-        smallerMatrix = smallerMatrix("UPLEFT", matrix, midRowIdx, midColIdx);
+        smallerMatrix = createSmallerMatrix("UPLEFT", matrix, midRowIdx, midColIdx);
         return searchInSortedMatrix(smallerMatrix, target)
     } else return [midRowIdx, midColIdx]
 }
@@ -42,6 +43,6 @@ const mat = [
     [99,100,103,106,128,1004]
 ]
 
-// console.log(searchInSortedMatrix(mat, 44))
+console.log(searchInSortedMatrix(mat, 44))
 
 console.log(createSmallerMatrix("DOWNRIGHT", mat, 2, 3))
