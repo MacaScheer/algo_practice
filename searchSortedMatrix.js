23fa2946760dead58a51edb38b3ba49a4e5ba750
Michael_Scheer <macas.scheer@gmail.com>
2020-05-18 13:59:38 -0700

    search sorted matrix algorithm with print statement of prompt

---
+++ searchSortedMatrix.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+'use strict';
+
+console.log(`search sorted matrix: given a two-dimensional array of distinct integers and a target integer, ${"\n"} write a function that returns the indices or row/column of the target number.${"\n"} Each row in the matrix is sorted and each column in the matrix is sorted. ${"\n"} The matrix does not necessarily have equal height and width.`);
+
+function searchInSortedMatrix(matrix) {
+    
+
+
+}
