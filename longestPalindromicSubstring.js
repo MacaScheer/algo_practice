e2778394c0d7fa62d850248beb31fdcf10962eac
Michael_Scheer <macas.scheer@gmail.com>
2020-05-18 19:26:52 -0700

    longest palindromic substring algorithm

---
+++ longestPalindromicSubstring.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+'use strict';
+
+console.log("Longest Palindromic Substring");
+
+function longestPalidromicSubstring(string) {
+    
+}
+
+console.log(longestPalidromicSubstring("abaxyzzyxf"), "should return 'xyzzyx'");
