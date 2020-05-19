63d75bae97ad3b265ad86f07ef42e674f7d9866f
Michael_Scheer <macas.scheer@gmail.com>
2020-05-18 19:16:41 -0700

    simpler approach with while loop

--- searchSortedMatrix.js
+++ searchSortedMatrix.js
@@ -6,6 +6,7 @@ console.log(`PSEUDOCODE: ${"\n"}I plan on using a sort of 2D binary search metho
 console.log(`CANNOT eliminate a quarter of the matrix on each call, because there isn't a way to tell (if the target is greater/less than the number in the row/col ${"\n"}being looked at, that the number may still be in the same row and not on the next column`)
 console.log("\n");
 console.log(`NEW APPROACH: compare the top right element to the target. ${"\n"}We know that the number that is smaller than that number but greater than its neighbor to the left is down and left one${"\n"}We should be searching in a diagonal direction through the matrix, possibly shrinking the array from the top right, down/left`)
+console.log("If target is greater than current value, search down or right, and vice versa")
 function searchInSortedMatrix(matrix, target) {
     let midColIdx = Math.floor(matrix[0].length / 2);
     let midRowIdx = Math.floor(matrix.length / 2);
@@ -62,13 +63,6 @@ function createSmallerMatrix(whichWay, matrix, col) {
     }
     return newMatrix
 }
-const mat = [
-    [1, 4, 7, 12, 15, 1000],
-    [2, 5, 19, 31, 32, 1001],
-    [3, 8, 24, 33, 35, 1002],
-    [40, 41, 42, 44, 45, 1003],
-    [99,100,103,106,128,1004]
-]
 
 // console.log(searchInSortedMatrix(mat, 44))
 
@@ -76,21 +70,48 @@ const mat = [
 
 // console.log(lookAtHalves(mat))
 
-function searchSortedMatrix(matrix, target, currIdx = [0,matrix.length - 1]) {
-    let topRight = matrix[currIdx[0]][currIdx[1]]
-    let bool = topRight > target;
-    let bool2 = topRight === target;
-    if (bool && !bool2) {
-        currIdx[0]++;
-        currIdx[1]--
-        searchSortedMatrix(matrix, target, currIdx)
-    } else if (!bool && !bool2) {
-        currIdx[0]++
-        searchSortedMatrix(matrix, target, currIdx)
-    } else {
-        return [0, matrix[0].length -1]
+// function searchSortedMatrix(matrix, target, currIdx = [0,matrix.length - 1]) {
+    
+    //     let value = matrix[currIdx[0]][currIdx[1]]
+    //     console.log("currIdx: ", currIdx, " value: ",value)
+    //     let greaterThanTarget = value > target;
+    //     let lessThanTarget = value < target;
+    //     // if(currIdx[0] > matrix.length - 1 || currIdx[1] < 0) return [-1,-1]
+    //     if (greaterThanTarget) {
+        //         currIdx[0]++;
+        //         currIdx[1]--
+        //         searchSortedMatrix(matrix, target, currIdx)
+        //     } else if (lessThanTarget) {
+            //         currIdx[0]++
+            //         searchSortedMatrix(matrix, target, currIdx)
+            //     } else {
+                //         return currIdx
+                //     }
+                // }
+                
+const mat = [
+[1, 4, 7, 12, 15, 1000],
+[2, 5, 19, 31, 32, 1001],
+[3, 8, 24, 33, 35, 1002],
+[40, 41, 42, 44, 45, 1003],
+[99,100,103,106,128,1004]
+]
+                
+function searchMatrix(matrix, target) {
+    let idx = [0, matrx[0].length - 1]
+    
+    while (idx[0] < matrix.length && idx[1] < matrix[0].length - 1) {
+        let val = matrix[idx[0]][idx[1]]
+        console.log(val)
+        if (target < val) {
+            idx[1]--
+        } else if (target > val){
+            idx[0]++
+        } else {
+            return idx
+        }
     }
+    return [-1,-1]
 }
-
-
-console.log(searchSortedMatrix(mat, 44))
+                
+console.log(searchMatrix(mat, 44))
