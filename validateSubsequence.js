0a7b21642f88f4269dd903f0033240ffed3d6090
Michael_Scheer <macas.scheer@gmail.com>
2020-05-20 13:03:38 -0700

    console log the prompt

--- validateSubsequence.js
+++ validateSubsequence.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 console.log("VALIDATE SUBSEQUENCE");
-
+console.log("given two non-empty arrays of integers, \n write a function that determines whether the second array is a subsequence of the first one \n a subsequence of an array is a set of numbers that aren't necessarily adjacent in the array \n but are in the same order as they appear in the array")
 
 function validateSubsequence(array, sequence) {
     
