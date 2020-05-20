6fcfb78b9bdec3c8850deb6a1601541c2ff17b11
Michael_Scheer <macas.scheer@gmail.com>
2020-05-20 14:01:47 -0700

    new approach which also mutates the input array

--- validateSubsequence.js
+++ validateSubsequence.js
@@ -9,21 +9,34 @@ function validateSubsequence(array, sequence) {
         el2 = sequence[i];
         el1 = sequence[i - 1]
         idx1 = array.indexOf(el1);
+        if (array[idx1] === null || array[idx1] === -1) {
+            return false;
+        } else {
+            array[idx1] = null;
+        }
         idx2 = array.indexOf(el2);
-        console.log("el1:",el1," el2:", el2, " idx1: ",idx1, "idx2: ", idx2, array)
-        if(array[idx1] === null || array[idx2] === null) return false
-        array[idx1] = null;
-        array[idx2] = null;
-        if(idx1 >= idx2 || idx1 === -1 || idx2 === -1) return false
+        if (array[idx2] === null || array[idx2] === -1) {
+            return false;
+        } else {
+            array[idx2] = null;
+        }
 
+        console.log("el1:",el1," el2:", el2, " idx1: ",idx1, "idx2: ", idx2, array)
+        // if(array[idx1] === null || array[idx2] === null) return false
+       
+        // array[idx2] = null;
+        if(idx1 >= idx2) return false
+        console.log(array)
     }
     console.log(array)
     return true;
 
 }
-
+console.log("NEW APPROACH, SPLICE OUT THE ELEMENT AS IT APPEARS IN BOTH ARRAYS")
 // console.log(validateSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]), "TRUE");
-console.log(validateSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 22, 6, -1, 8, 10]), "FALSE");
+// console.log(validateSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 22, 6, -1, 8, 10]), "FALSE");
 
 
-console.log([5, 1, 22, 25, 6, -1, 8, 10].indexOf(-1))
+// console.log([5, 1, 22, 25, 6, -1, 8, 10].indexOf(-1))
+
+console.log(validateSubsequence([1,1,1,1,1], [1,1,1]), "should be true")
