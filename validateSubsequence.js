353cadcc82cb2029edd86b317ebcf3b1d8369d23
Michael_Scheer <macas.scheer@gmail.com>
2020-05-20 13:01:05 -0700

    validateSubsequnce algorithm from algoexpert

---
+++ validateSubsequence.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+'use strict';
+console.log("VALIDATE SUBSEQUENCE");
+
+
+function validateSubsequence(array, sequence) {
+    
+
+}
+
+console.log(validateSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10]), "TRUE");
