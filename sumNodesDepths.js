025439c7861aeb0456342a115acf914e92e10cd1
Michael_Scheer <macas.scheer@gmail.com>
2020-05-20 16:50:56 -0700

    nodes depths algorithm

---
+++ sumNodesDepths.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+'use strict';
+
+console.log("SUM NODE'S DEPTHS");
+
+function nodeDepths(root) {
+    
+
+}
+
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+let A = new BinaryTree(1);
+let B = new BinaryTree(2)
+let C = new BinaryTree(3)
+let D = new BinaryTree(2)
+let B = new BinaryTree(2)
+let B = new BinaryTree(2)
+let B = new BinaryTree(2)
+let B = new BinaryTree(2)
+let B = new BinaryTree(2)
