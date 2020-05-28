32d9c59d4c8268b1de63248c3ee25b6308fdcd05
Michael_Scheer <macas.scheer@gmail.com>
2020-05-27 20:11:51 -0700

    finish counting sort algorithm

--- countingSort.js
+++ countingSort.js
@@ -5,13 +5,18 @@ console.log("COUNTING SORT: \n PSEUDO CODE: (first for strictly positive integer
 
 function countingSort(arr, max) {
     if (arr.length === 0) return [];
+    let output = [];
     const countingArr = new Array(max + 1).fill(0);
     for (let i = 0; i < arr.length; i++){
         let el = arr[i];
         console.log(el)
         countingArr[el]++
     }
-    console.log(countingArr)
+    for (let i = 0; i < countingArr.length; i++){
+        let el2 = countingArr[i];
+        if(el2 !== 0) output.push(i)
+    }
+    console.log(output)
 }
 
 console.log(countingSort([20,19,17,18,13,14,15,1,2,4,3,6,5,5,8,9,10,10],20))
