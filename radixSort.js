1b7c0cd646df80d6344bff17e69cfd503dbc20bd
Michael_Scheer <macas.scheer@gmail.com>
2020-05-28 14:00:41 -0700

    deduced that array.fill([]) creates identical arrays with same object id

--- radixSort.js
+++ radixSort.js
@@ -5,15 +5,25 @@ console.log("radix sort");
 
 
 function radixSort(array) {
-    const buckets = new Array(10)
+    const buckets = new Array(10).fill([]);
+    // console.log(buckets[4])
     for (let i = 0; i < array.length; i++){
-        let last = lastDigit(array[i]);
-        buckets[last] = array[i];
+        let el = array[i]
+        let last = lastDigit(el);
+        buckets[last].push(el)
+        console.log(last, el, buckets[last], buckets)
+        
     }
-    console.log(buckets)
+    // console.log(buckets)
+    let nextList = [];
+    // while (buckets.length) {
+    //     let el = buckets.shift();
+    //     nextList.push(...el)
+    // }
+    // console.log(nextList)
 }
 
-function lasDigit(num) {
+function lastDigit(num) {
     return num % 10;
 }
 
