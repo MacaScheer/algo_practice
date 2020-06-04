9a1d60548761760869dd963f27601cabae486561
Michael_Scheer <macas.scheer@gmail.com>
2020-06-04 13:57:09 -0700

    populate suffix trie from string function and helper function

--- suffixTrieAlgos.js
+++ suffixTrieAlgos.js
@@ -1,3 +1,37 @@
 #!/usr/bin/env node
 'use strict';
-console.log("SuffixTrieAlgos")
+// console.log("SuffixTrieAlgos")
+
+const trie = {
+    "c": { "*": true },
+    "b": {
+        "c": { "*": true },
+        "a": { "b": { "c": { "*": true } } },
+    },
+    "a":{"b":{"c":{"*":true}}}
+}
+
+class SuffixTrie{
+    constructor(string) {
+        this.root = {};
+        this.endSymbol = "*";
+        this.populateSuffixTrieFrom(string)
+    }    
+    populateSuffixTrieFrom(string) {
+        for (let i = 0; i < string.length; i++){
+            this.insertSubstring(string, i);
+        }
+    }
+    insertSubstring(string, i) {
+        let node = this.root;
+        for (let j = i; j < string.length; j++){
+            let char = string[j]
+            if (!node[char]) node[char] = {};
+            node = node[char]
+        }
+        node['*'] = true;
+    }
+}
+
+let sufX = new SuffixTrie('babc')
+console.log(sufX)
