94ccc3b59d4ab0d72400e7c82bf63d433db6cbf0
Michael_Scheer <macas.scheer@gmail.com>
2020-06-22 19:45:32 -0700

    quickSort linear time algorithm, start

---
+++ quickSelect.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+'use strict';
+
+console.log("quickSelect should iterate through an array and return the Kth largest integer in the array, in O(n) time")
+function quickSelect(array, k) {
+    
+}
