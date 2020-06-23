5bab668c5b75cd3e68f30293fbae53f3eb9757d4
Michael_Scheer <macas.scheer@gmail.com>
2020-06-23 14:04:03 -0700

    quickSELECT is correct implementation on average time O(n)

--- quickSelect.js
+++ quickSelect.js
@@ -19,11 +19,12 @@ THE PIVOT IN ITS FINAL POSITION. IF THE PIVOT IS THE KTH POSITION, YOU'RE DONE
 IF NOT, FIGURE OUT IF THE KTH SMALLEST IS TO THE LEFT OR RIGHT OF THE PIVOT
 /*/
 function quickSelect(array, k) {
-    // if (array.length < k) return 
+    // if (array.length < k) return -1
     let piv = array[0];
     let lIdx = 1;
     let rIdx = array.length - 1;
-    while (lIdx <= rIdx) {
+    // while (lIdx <= rIdx) {
+    while(lIdx <= rIdx){
         let left = array[lIdx];
         let right = array[rIdx];
         if (left > piv && right < piv) {
@@ -35,15 +36,67 @@ function quickSelect(array, k) {
         }
     }
     [array[0], array[rIdx]] = [array[rIdx], array[0]]
-    if (rIdx === k) return array[k]
     console.log(array)
-    while (k < rIdx) {
-        
+
+
+    if (k-1 >= rIdx) {
+        return array[k-1]
+    } else {
+        let newArr = array.slice(0, rIdx - 1)
+        quickSelect(newArr, k)
     }
-    while (k > rIdx) {
-        
-    }
-    
+    // while (k < rIdx) {
+    //     let left = array[lIdx];
+    //     let right = array[rIdx];
+    //     if (left > piv && right < piv) {
+    //         [array[lIdx], array[rIdx]] = [array[rIdx], array[lIdx]]
+    //     } else if (left <= piv) {
+    //         lIdx++
+    //     } else if (right >= piv) {
+    //         rIdx--
+    //     }
+    // }
+    return array[k]
+}
+
+
+function quickSELECT(array, k) {
+    const position = k - 1;
+    return quickSelectHelper(array, 0, array.length - 1, position)
+}
+
+function quickSelectHelper(array, startIdx, endIdx, position) {
+    while (true) {
+        if (startIdx > endIdx) {
+            throw new Error('Your algorithm should never arrive here')
+        }
+        const pivotIdx = startIdx;
+        let leftIdx = startIdx + 1;
+        let rightIdx = endIdx;
+        while (leftIdx <= rightIdx) {
+            if (array[leftIdx] > array[pivotIdx] && array[rightIdx] < array[pivotIdx]) {
+                swap(leftIdx, rightIdx, array)
+            }
+            if (array[leftIdx] < array[pivotIdx]) {
+                leftIdx++
+            }
+            if (array[rightIdx] > array[pivotIdx]) {
+                rightIdx--
+            }
+        }
+        swap(pivotIdx, rightIdx, array)
+            if (rightIdx === position) {
+                return array[rightIdx]
+            } else if (rightIdx < position) {
+                startIdx = rightIdx + 1
+            } else {
+                endIdx = rightIdx - 1;
+            }
+        }
+}
+
+function swap(i, j, array) {
+    return [array[i], array[j]] = [array[j], array[i]]
 }
 
 // function compareSidesLoop(lIdx, rIdx, array) {
@@ -60,8 +113,8 @@ function quickSelect(array, k) {
 //     }
 // }
 
-
-console.log(quickSelect([22,-90,5,77,-45,0,-1,33,7,64,80]))
+// [-90,-45,-1,0,5,7,22,33,64,77,80]
+console.log(quickSELECT([22,-90,5,77,-45,0,-1,33,7,64,80], 4))
 function minArr(idx1, idx2, arr) {
     if (arr[idx1] > arr[idx2]) {
         return [arr[idx2], arr[idx1]]
