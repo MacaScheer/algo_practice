9505e8e89f11b17300d0acba42363b225a5e19fb
Michael_Scheer <macas.scheer@gmail.com>
2020-06-25 18:03:54 -0700

    getMinSpaces helper function recursion set up

--- numbersInPi.js
+++ numbersInPi.js
@@ -8,12 +8,35 @@ console.log(`Given a string representation of the first n digits of Pi
             resulting numbers are found in the list of integers.  
             If no number of spaces to be added exists such that all 
             resulting numbers are found in the list of integers, return -1`)
-            console.log("Big O of space is going to be O(n + m) space \n Big O of time is going to be O(n**3) because of the slicing and the nested loop of all prefixes")
+console.log(`Big O of space is going to be O(n + m) space 
+            Big O of time is going to be O(n**3) because
+            of the slicing and the nested loop of all prefixes`)
 // HASH TABLE USED
-
+// OR IS IT JUST AN OBJECT?
 
 function numbersInPi(pi, numbers) {
-    
+    // build hash table, just an object..
+    // let numbersTable = {}
+    // for (let number in numbers) {
+    //     numbersTable[number] = true
+    // }
+    let numbersTable = numbers.reduce((o, key) => Object.assign(o, { [key]: true }), {})
+    let minSpaces = getMinSpaces(pi, numbersTable, {}, 0)
+}
+
+function getMinSpaces(pi, numbersTable, cache, idx) {
+    // return negative one because you have already added one in the recursive call, assuming there will be a place for another space
+    if (idx === pi.length) return -1
+    if (idx in cache) return cache[idx];
+    let minSpaces = Infinity;
+    for (let i = idx; i <= pi.length; i++){
+        let prefix = pi.slice(idx, i + 1)
+        if (prefix in numbersTable) {
+            let minSpacesInSuffix = getMinSpaces(pi, numbersTable, cache, i + 1)
+            minSpaces = Math.min(minSpaces, minSpacesInSuffix + 1)
+            // let suffix = pi.slice(i)
+        }
+    }
 }
 
 console.log(numbersInPi("3141592653589793238462643383279", ["314159265358979323846", "26433","8","3279","314159265","35897932384626433832","79"]), "should be 2")
