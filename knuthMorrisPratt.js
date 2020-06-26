4f0665f38b240c087686c2b7c607a229c0ad9520
Michael_Scheer <macas.scheer@gmail.com>
2020-06-26 11:58:31 -0700

    Knuth-Morris-Pratt algorithm

---
+++ knuthMorrisPratt.js
@@ -0,0 +1,8 @@
+// Knuth-Morris-Pratt Algorithm
+// keeping track of repeated patterns
+// optimal for doing gene mapping, or identifying genetic details
+
+function knuthMorrisPratt(string, subString) {
+    
+}
+console.log(knuthMorrisPratt("stringdds1", "stringdds1"))
--- numbersInPi.js
+++ numbersInPi.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log(`Given a string representation of the first n digits of Pi  
-            and a list of positive integers (all in string format 
-            write a function that returns the smallest number of spaces 
-            that can be added to the n digits of Pi such that all 
-            resulting numbers are found in the list of integers.  
-            If no number of spaces to be added exists such that all 
-            resulting numbers are found in the list of integers, return -1`)
-console.log(`Big O of space is going to be O(n + m) space 
-            Big O of time is going to be O(n**3 + m) because
-            of the slicing and the nested loop of all prefixes`)
+// console.log(`Given a string representation of the first n digits of Pi  
+//             and a list of positive integers (all in string format 
+//             write a function that returns the smallest number of spaces 
+//             that can be added to the n digits of Pi such that all 
+//             resulting numbers are found in the list of integers.  
+//             If no number of spaces to be added exists such that all 
+//             resulting numbers are found in the list of integers, return -1`)
+// console.log(`Big O of space is going to be O(n + m) space 
+//             Big O of time is going to be O(n**3 + m) because
+//             of the slicing and the nested loop of all prefixes`)
 // HASH TABLE USED
 // OR IS IT JUST AN OBJECT?
 
@@ -42,4 +42,5 @@ function getMinSpaces(pi, numbersTable, cache, idx) {
     return cache[idx]
 }
 
-console.log(numbersInPi("3141592653589793238462643383279", ["314159265358979323846", "26433","8","3279","314159265","35897932384626433832","79"]), "should be 2")
+console.log(numbersInPi("3141592653589793238462643383279", ["314159265358979323846", "26433", "8", "3279", "314159265", "35897932384626433832", "79"]), "should be 2")
+console.log(numbersInPi("3141592653589793238462643383279",["3141592653589793238462643383279"]))
