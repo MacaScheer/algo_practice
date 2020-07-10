bf4c97fd586c950a7ad7d6bdc77b96278be48548
Michael_Scheer <macas.scheer@gmail.com>
2020-07-09 17:43:07 -0700

    hasPalindromPermutation algorithm O(n)

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+'use strict'
+
 let word = "abcdcba";
 let word2 = "abcdefedcbi";
 
@@ -10,4 +13,48 @@ const isPalindrome = function(str) {
   return true;
 };
 
-console.log(isPalindrome(word2));
+// console.log(isPalindrome(word2));
+
+
+// function hasPalindromePermutation(string) {
+//   let charSet = {}
+//   for (let i = 0; i < string.length; i++){
+//     let char = string[i];
+//     charSet[char] === undefined ? charSet[char] = 1 : charSet[char] += 1
+//   }
+//   return charCount(charSet)
+// }
+
+// function charCount(set) {
+//   let evenCount = 0; let oddCount = 0;
+//   for (let key in set) {
+//     console.log("set[key]: ",set[key])
+//     set[key] % 2 === 0 ? evenCount += set[key] : oddCount += set[key]
+//   }
+//   console.log(evenCount, " oddCount: ",oddCount)
+//   if (oddCount % 2 === 0) {
+//     return true
+//   } else {
+//      return false
+//   }
+// }
+
+
+function hasPalindromePermutation(string) {
+  let charSet = new Set();
+  for (let i = 0; i < string.length; i++){
+    let char = string[i];
+    if (!charSet.has(char)) {
+      charSet.add(char)
+    } else {
+      charSet.delete(char)
+    }
+  }
+  return charSet.size <= 1
+}
+console.log(hasPalindromePermutation("civic"), " is true")
+console.log(hasPalindromePermutation("ivicc"), " is true")
+console.log(hasPalindromePermutation("civil"), " is false")
+console.log(hasPalindromePermutation("livci"), " is false");
+
+// lirouruoril
