9584c358c47138070d911ecb2ff69d03ad27cc05
Michael_Scheer <macas.scheer@gmail.com>
2020-07-18 17:34:54 -0700

    minRewards algorithm

---
+++ minRewards.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+'use strict';
+
+
+function minRewards(scores) {
+    
+
+}
+const score = [8, 4, 2, 1, 3, 6, 7, 9, 5]
+
+console.log(minRewards(score), " should be 25, because [4,3,2,1,2,3,4,5,1]")
