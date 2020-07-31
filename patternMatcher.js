3e602bd7f05cfecace6e95c24d52984706e30b38
Michael_Scheer <macas.scheer@gmail.com>
2020-07-30 17:14:13 -0700

    first pass of patternMatcher solution, possible edgecases not accounted for

--- patternMatcher.js
+++ patternMatcher.js
@@ -34,64 +34,40 @@ function patternMatcher(pattern, string) {
     let [newPattern, side, firstPosition] = patternSwap(pattern);
     let table = hashTable(pattern)
     let possibleString = ''
-    let len = 1;
-
+    let lenOfX = 1;
+    let firstYIdx = pattern.indexOf('y')
     if (table['y'] !== undefined) {
-        while (len * pattern.length < string.length) {
-            if(possiblePartialString)
-            let substring = string.slice(0,len)
+        while (lenOfX * pattern.length < string.length) {
+            // if(possibleString)
+            let substring = string.slice(0,lenOfX)
             let x = substring;
-            let possiblePartialString = replaceWithSubString(pattern, x, 'x')
             //'ggyggy'
-            let firstNonX = firstNonChar(possiblePartialString, x)
-            console.log(possiblePartialString)
-            len++
+            let yIdx = lenOfX * firstYIdx
+            // console.log("subStringX: ", x, 'yIdx: ', yIdx)
+            let lenOfY = lengthOfY(table, pattern, lenOfX, string)
+            let y = string.slice(yIdx, lenOfY + yIdx)
+            console.log('x: ', x, ' y:',y)
+            let possibleString = replaceWithSubString(pattern, x, y)
+            if (possibleString === string){
+                if(side) return [x,y] 
+                return [y,x]
+            }
+            // x = g, y = string.slice(yIdx, lenOfY) = 'gopowerranger' 
+            
+            
+            // if (x.length * table['x'] + lenOfY * table['y'] === string.length) {
+            //     return finalArr(x, string, lenOfY, table)
+            // }
+            // let firstNonX = firstNonChar(possibleString, x)
+            // console.log(possibleString)
+            lenOfX++
         }
-    } else {
-
-    }
-
+    } 
+    return false
     // if (side) {
     // } else {
     // }
 }
-function firstNonXChar(string, x) {
-    let i = 0; 
-    let char = string[i]
-    let xLength = x.length;
-    while (i < string.length && char === x) {
-        let nextChunk = string.slice(i, xLength);
-        console.log("nextChunk: ",nextChunk)
-        if (nextChunk !== x) return i;
-        i = xLength + 1
-    }
-}
-
-function replaceWithSubString(pattern, subString, letter) {
-    let newString = [];
-    for (let i = 0; i < pattern.length; i++){
-        let patternChar = pattern[i];
-        if (patternChar === letter) {
-            newString.push(subString)
-        } else {
-            newString.push(patternChar)
-        }
-    }
-    return newString.join('')
-}
-
-// function findFirstPattern(string, substring) {
-//     // substring = 'go'
-//     //pattern = 'xxy'
-
-//     let i = 0;
-//     while (i < string.length) {
-        
-
-//         i++
-//     }
-
-// }
 
 function hashTable(pattern) {
     let table = {}
@@ -122,6 +98,65 @@ function patternSwap(pattern) {
     } 
     return [false, newPattern, newPattern.indexOf('y')]
 }
+function replaceWithSubString(pattern, subStringX, subStringY) {
+    let newString = "";
+    for (let i = 0; i < pattern.length; i++){
+        let patternChar = pattern[i];
+        if (patternChar === 'x') {
+            newString += subStringX
+        } else {
+            newString += subStringY
+        }
+    }
+    return newString
+}
+
+function finalArr(x, string, lenOfY, table) {
+    let arr = [x];
+    let ys = [];
+
+    for (let i = 0; i < string.length; i++){
+        let chunk = string.slice(i, x.length);
+        let nextidx = chunk.indexOf(x);
+        ys.push()
+    }
+}
+
+
+function lengthOfY(table, pattern, xLength, string) {
+    let totalXLength = table['x'] * xLength;
+    let remainderLength = (string.length - totalXLength) 
+    let yLength = remainderLength / table['y']
+    console.log("totalXLength: ", totalXLength, " remainderLength: ", remainderLength, " table: ", table, ' yLength: ', yLength)
+    if (remainderLength % table['y'] !== 0) return false
+    return yLength
+}
+function firstNonChar(string, x) {
+    let i = 0; 
+    let char = string[i]
+    let xLength = x.length;
+    while (i < string.length && char === x) {
+        let nextChunk = string.slice(i, xLength);
+        console.log("nextChunk: ",nextChunk)
+        if (nextChunk !== x) return i;
+        i = xLength + 1
+    }
+}
+
+
+// function findFirstPattern(string, substring) {
+//     // substring = 'go'
+//     //pattern = 'xxy'
+
+//     let i = 0;
+//     while (i < string.length) {
+        
+
+//         i++
+//     }
+
+// }
+
 
 
 // console.log(hashTable('xxxxx')['y'])
@@ -151,4 +186,4 @@ function patternSwap(pattern) {
 
 let st1 = "gogopowerrangergogopowerranger";
 let pattern1 = "xxyxxy"
-console.log(patternMatcher(pattern1, st1), `output shoulw match: ${["go", "powerranger"]}`)
+console.log(patternMatcher(pattern1, st1), `output should match: ["go", "powerranger"]`)
