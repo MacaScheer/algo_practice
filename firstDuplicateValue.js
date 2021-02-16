5e111876d3acb721af57dce7bec509b07a7009b0
Michael_Scheer <macas.scheer@gmail.com>
2021-02-16 11:33:59 -0800

    easy algo first dup

---
+++ firstDuplicateValue.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+'use strict'
+
+/*
+Given an array of integers between 1 and n inclusive, where n is the length of
+the array, write a function that returns the first integer that appears more 
+than once (when read from left to right)
+*/
+
+function firstDuplicateValue(arr) {
+    let vals = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (vals[arr[i]]) {
+            return arr[i];
+        } else {
+            vals[arr[i]] = true;
+        }
+    }
+}
+
+console.log(firstDuplicateValue([2, 1, 5, 2, 3, 3, 4]), "should be 2")
